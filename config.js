c6127746e518943e35a32d6902c77d0af273c44a
jinhoyoho <jinho7018@naver.com>
2025-02-04 21:44:09 +0900

    Fix: 공유 사진 변경

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const WEDDING_INVITATION_URL = "https://haeunbirthday.netlify.app/";
 export const KAKAOTALK_API_TOKEN = "1ac590c836fc6b08675cacbed8e850ef";
 export const KAKAOTALK_SHARE_IMAGE =
-  "https://github.com/jinhoyoho/Haeun_doll/blob/main/src/assets/haeun_pic1.jpg?raw=true";
+  "https://github.com/jinhoyoho/Haeun_doll/blob/main/src/assets/hauen_main.jpg?raw=true";
 
 export const WEDDING_DATE = "2025년 02월 23일, 일요일 오후 5시";
 export const WEDDING_LOCATION = "파티수 파주한옥점";
